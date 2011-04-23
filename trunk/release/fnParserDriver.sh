d9f2eb949057fcf2d8a6e1129b609b281667db3c
dipanjand@gmail.com <dipanjand@gmail.com@29867d48-12e9-6f0d-cb52-f4a5b6b427cd>
2011-04-23 19:47:28 +0000

    Added final scripts to the release directory.

--- trunk/release/fnParserDriver.sh
+++ trunk/release/fnParserDriver.sh
@@ -42,16 +42,129 @@ else
    REMOVE_DOT_TMP=0
 fi
 
-# run semafor script
-/bin/sh ${SEMAFOR_HOME}/release/runFNParser.sh \
-"${SEMAFOR_HOME}" \
-"${TEMP_DIR}" \
-"${INPUT_FILE}" \
-"${OUTPUT_FILE}" \
-"${MST_PARSER_HOME}" \
-"${JAVA_HOME_BIN}" \
-"${MODEL_DIR}" \
-${MST_PORT}
+CLASSPATH=".:${SEMAFOR_HOME}/lib/semafor-deps.jar"
+
+rm -f ${INPUT_FILE}.tokenized
+if [ "${GOLD_TARGET_FILE}" == "null" ]
+then
+    echo "**********************************************************************"
+    echo "Tokenizing file: ${INPUT_FILE}"
+    sed -f ${SEMAFOR_HOME}/scripts/tokenizer.sed ${INPUT_FILE} > ${INPUT_FILE}.tokenized
+    echo "Finished tokenization."
+    echo "**********************************************************************"
+    echo
+    echo
+else
+    echo "**********************************************************************"
+    echo "Gold target file provided, not tokenizing input file."
+    cat ${INPUT_FILE} > ${INPUT_FILE}.tokenized
+    echo "**********************************************************************"
+    echo 
+    echo
+fi
+
+echo "**********************************************************************"
+echo "Part-of-speech tagging tokenized data...."
+rm -f ${INPUT_FILE}.pos.tagged
+cd ${SEMAFOR_HOME}/scripts/jmx
+./mxpost tagger.project < ${INPUT_FILE}.tokenized > ${INPUT_FILE}.pos.tagged
+echo "Finished part-of-speech tagging."
+echo "**********************************************************************"
+echo
+echo
+
+rm -f ${INPUT_FILE}.conll.input
+rm -f ${INPUT_FILE}.conll.output
+if [ "$MST_MODE" != "server" ]
+then
+    echo "**********************************************************************"
+    echo "Preparing the input for MST Parser..."
+    cd ${SEMAFOR_HOME}
+    ${JAVA_HOME_BIN}/java \
+	-classpath ${CLASSPATH} \
+	edu.cmu.cs.lti.ark.fn.data.prep.CoNLLInputPreparation \
+	${INPUT_FILE}.pos.tagged ${INPUT_FILE}.conll.input
+
+    echo "Dependency parsing the data..."
+    cd ${MST_PARSER_HOME}
+    ${JAVA_HOME_BIN}/java -classpath ".:./lib/trove.jar:./lib/mallet-deps.jar:./lib/mallet.jar" \
+	-Xms8g -Xmx8g mst.DependencyParser \
+	test separate-lab \
+	model-name:${MODEL_DIR}/wsj.model \
+	decode-type:proj order:2 \
+	test-file:${INPUT_FILE}.conll.input \
+	output-file:${INPUT_FILE}.conll.output \
+	format:CONLL
+    echo "Finished dependency parsing."
+    echo "**********************************************************************"
+    echo
+    echo
+fi
+
+if [ "${AUTO_TARGET_ID_MODE}" == "relaxed" ]
+then 
+    RELAXED_FLAG=yes
+else
+    RELAXED_FLAG=no
+fi
+
+
+if [ "${USE_GRAPH_FILE}" == "yes" ]
+then
+    GRAPH_FILE=${MODEL_DIR}/graph.gz
+else
+    GRAPH_FILE=null
+fi
+
+ALL_LEMMA_TAGS_FILE=${INPUT_FILE}.all.lemma.tags
+
+
+echo "**********************************************************************"
+echo "Performing frame-semantic parsing"
+cd ${SEMAFOR_HOME}
+${JAVA_HOME_BIN}/java \
+    -classpath ${CLASSPATH} \
+    -Xms4g -Xmx4g \
+    edu.cmu.cs.lti.ark.fn.parsing.ParserDriver \
+    mstmode:${MST_MODE} \
+    mstserver:${MST_MACHINE} \
+    mstport:${MST_PORT} \
+    posfile:${INPUT_FILE}.pos.tagged \
+    test-parsefile:${INPUT_FILE}.conll.output \
+    stopwords-file:${SEMAFOR_HOME}/stopwords.txt \
+    wordnet-configfile:${SEMAFOR_HOME}/file_properties.xml \
+    fnidreqdatafile:${MODEL_DIR}/reqData.jobj \
+    goldsegfile:${GOLD_TARGET_FILE} \
+    userelaxed:${RELAXED_FLAG} \
+    testtokenizedfile:${INPUT_FILE}.tokenized \
+    idmodelfile:${MODEL_DIR}/idmodel.dat \
+    alphabetfile:${MODEL_DIR}/parser.conf \
+    framenet-femapfile:${MODEL_DIR}/framenet.frame.element.map \
+    eventsfile:${INPUT_FILE}.events.bin \
+    spansfile:${INPUT_FILE}.spans \
+    model:${MODEL_DIR}/argmodel.dat \
+    useGraph:${GRAPH_FILE} \
+    frameelementsoutputfile:${INPUT_FILE}.fes \
+    alllemmatagsfile:${ALL_LEMMA_TAGS_FILE}
+
+end=`wc -l ${INPUT_FILE}.tokenized`
+end=`expr ${end% *}`
+echo "Producing final XML document:"
+${JAVA_HOME_BIN}/java -classpath ${CLASSPATH} \
+    -Xms4g -Xmx4g \
+    edu.cmu.cs.lti.ark.fn.evaluation.PrepareFullAnnotationXML \
+    testFEPredictionsFile:${INPUT_FILE}.fes \
+    startIndex:0 \
+    endIndex:${end} \
+    testParseFile:${ALL_LEMMA_TAGS_FILE} \
+    testTokenizedFile:${INPUT_FILE}.tokenized \
+    outputFile:${OUTPUT_FILE}
+
+
+echo "Finished frame-semantic parsing."
+echo "**********************************************************************"
+echo
+echo
 
 # clean up
 if [ ${REMOVE_DOT_TMP} = 1 ]; then
